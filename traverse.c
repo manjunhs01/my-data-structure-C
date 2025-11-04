5b44344ececd8aecf05ba8af9bbdce6a04ebffb7
manjunhs01 <manjunhs28@gmail.com>
2025-11-04 17:46:48 +0530

    Update array size and print all elements

--- traverse.c
+++ traverse.c
@@ -1,9 +1,26 @@
+
 #include<stdio.h>
-int main(){
-    int a[] = {10,20,30,40,50};
-    int n = (sizeof(a)/sizeof(a[0]));
+int main()
+{
+    int a[]={10,20,30,40,50,60,70,80,90,100};
+    int n=sizeof(a)/sizeof(a[0]);
     printf("%d\n",n);
-    for(int i=0; i<n;i++){
-        printf(%d ",a[i]);
-    }
+    for(int i=0;i<n;i++){
+    printf("%d ",a[i]);
 }
+}
+output:
+10
+10 20 30 40 50 60 70 80 90 100 #include<stdio.h>
+int main()
+{
+    int a[]={10,20,30,40,50,60,70,80,90,100};
+    int n=sizeof(a)/sizeof(a[0]);
+    printf("%d\n",n);
+    for(int i=0;i<n;i++){
+    printf("%d ",a[i]);
+}
+}
+output:
+10
+10 20 30 40 50 60 70 80 90 100 
